93cd78f4de8ba0ea55081b65c259e0bd1e776239
Noury Hazem <88968358+Nouryhazem@users.noreply.github.com>
2026-09-01 18:42:42 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/weeding-invitation/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
